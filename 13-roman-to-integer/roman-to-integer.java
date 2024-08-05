fe0a379a9a49ea7c42f60cea2303d09f05220374
Manju S <146159668+Manju-Sivasubramani@users.noreply.github.com>
2024-08-05 14:20:54 +0530

    Time: 2 ms (100.00%) | Memory: 44.6 MB (47.13%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int romanToInt(String s) {
+     int ans=0;
+	 for(int i=s.length()-1;i>=0;i--) {
+		 char ch=s.charAt(i);
+		 int n=0;
+	
+		 switch(ch) {
+		 case 'I':
+			 n=1;break;
+		 case 'V':
+			 n=5;break;
+		 case 'X':
+			 n=10;break;
+		 case 'L':
+			 n=50;break;
+		 case 'C':
+			 n=100;break;
+		 case 'D':
+			 n=500;break;
+		 case 'M':
+			 n=1000;break;
+		 
+		 }
+		 if(4*n<ans)
+			 ans-=n;
+		 else 
+			 ans+=n;
+	    }
+        return ans;
+    }	 
+}
