ef7f0a7bd2d1d51608993788194e8a50b0b3f593
Manju S <146159668+Manju-Sivasubramani@users.noreply.github.com>
2024-08-17 14:46:51 +0530

    Time: 605 ms (14.14%) | Memory: 45.1 MB (28.67%) - LeetSync

---
+++ 214-shortest-palindrome/shortest-palindrome.java
@@ -0,0 +1,31 @@
+class Solution {
+    public Boolean isPalindrome(String s){
+        int i=0;
+        int j=s.length()-1;
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+    public String shortestPalindrome(String s) {
+        if(s.length()<=1){
+            return s;
+        }
+        int longestPalindromeEnd = 0;
+        for(int i=s.length();i>=0;i--){
+            if(isPalindrome(s.substring(0,i))){
+                longestPalindromeEnd=i;
+                break;
+            }
+        }
+        String suffix = s.substring(longestPalindromeEnd);
+        String reversedSuffix = new StringBuilder(suffix).reverse().toString();
+
+       
+        return reversedSuffix + s;
+    }
+}
