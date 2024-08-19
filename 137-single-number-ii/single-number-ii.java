ca37e749660c3f3e39f0dec300db6d5c60d705a9
Manju S <146159668+Manju-Sivasubramani@users.noreply.github.com>
2024-08-19 12:32:45 +0530

    Time: 92 ms (5.13%) | Memory: 45.1 MB (78.23%) - LeetSync

--- 137-single-number-ii/single-number-ii.java
+++ 137-single-number-ii/single-number-ii.java
@@ -2,7 +2,6 @@ class Solution {
     public int singleNumber(int[] nums) {
         int n=nums.length;
         int num=0;
-        int val=0;
         for(int i=0;i<n;i++){
             num=nums[i];
             int flag=0;
@@ -15,9 +14,9 @@ class Solution {
                 }
             }
             if(flag==0){
-                val= num;
+                break;
             }
         }
-        return val;
+        return num;
     }
 }
