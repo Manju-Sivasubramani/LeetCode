bbc60acb035138de030029c877622dcc1015ccd3
Manju S <146159668+Manju-Sivasubramani@users.noreply.github.com>
2024-08-21 14:47:06 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (48.31%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -2,7 +2,10 @@ class Solution {
     public int searchInsert(int[] nums, int target) {
         int n=nums.length;
         int start=0;
-        while(start<n&&nums[start]<target){
+        while(start<n&&nums[start]<=target){
+            if(nums[start]==target){
+                return start;
+            }
             start++;
         }
         return start;
